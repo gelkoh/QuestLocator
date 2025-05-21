57bf87e4a3aba7c0aa77b90980777e6c11d5acc9
Loris1109 <Lorenz.steiniger-Brach@student.htw-berlin.de>
2025-05-22 00:58:22 +0200

    working Word selection

---
+++ Assets/Scripts/ScalePanel.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScalePanel : MonoBehaviour
+{
+    Vector2 orgSize;
+
+    void Start()
+    {
+        orgSize = this.GetComponent<RectTransform>().sizeDelta;
+
+    }
+    public void reduceSize(GameObject obj)
+    {
+        if (obj != null && obj.GetComponent<RectTransform>() != null)
+        {
+            float width = obj.GetComponent<RectTransform>().sizeDelta.x;
+            float padding = this.GetComponent<HorizontalLayoutGroup>().padding.left + this.GetComponent<HorizontalLayoutGroup>().padding.right;
+            this.GetComponent<RectTransform>().sizeDelta = new Vector2(width + padding, orgSize.y);
+        }
+        else
+        {
+            Debug.LogWarning("NO RectTransform Connected");
+        }
+    }
+    public void expandSize()
+    {
+        this.GetComponent<RectTransform>().sizeDelta = orgSize;
+    }
+}
