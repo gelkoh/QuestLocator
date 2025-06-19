1e411682e5c4fff766281b4e16e29d9f17867427
Loris1109 <Lorenz.steiniger-Brach@student.htw-berlin.de>
2025-06-19 20:00:09 +0200

    ParentPanel, GeminiPanel, TitlePanel and Ingredients

--- Assets/_QuestLocator/Features/UI/TestPannels/IngredientPannel.cs
+++ Assets/_QuestLocator/Features/UI/TestPannels/IngredientPannel.cs
@@ -1,41 +1,46 @@
 using System;
-using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using TMPro;
-using UnityEngine.UI;
+using Unity.XR.CoreUtils;
+using UnityEngine;
 
 public class IngredientPannel : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private ProductParent productDisplayScript;
     [SerializeField] TextMeshProUGUI title;
-    [SerializeField] GameObject IngredientsSection;
-    [SerializeField] Image vLable;
+    [SerializeField] GameObject wordButtonPrefab;
+    Transform ingredientTransform;
+    [SerializeField] GameObject content;
+    [SerializeField] GameObject zutatenListe;
+    List<GameObject> wordButtonList = new List<GameObject>();
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         productDisplayScript = GetComponentInParent<ProductParent>();
+        content = zutatenListe.GetNamedChild("Content");
+        ingredientTransform = content.transform;
         FillInfo();
     }
-    // Update is called once per frame
+
     private void FillInfo()
     {
         title.text = productDisplayScript.productData.Product.ProductName;
 
         foreach (var ingredient in productDisplayScript.productData.Product.Ingredients)
         {
-            GameObject textObj = new GameObject("IngredientText");
-            textObj.transform.SetParent(IngredientsSection.transform, false); // 'false' keeps local scale
-
-            // Add TextMeshProUGUI component
-            var tmp = textObj.AddComponent<TextMeshProUGUI>();
-            tmp.text = ingredient.Id[3..];
-            tmp.fontSize = 24;
-            tmp.alignment = TextAlignmentOptions.MidlineLeft;
-            tmp.enableAutoSizing = true;
-
-            // Optionally, add LayoutElement if you want to control spacing/size
-            var layout = textObj.AddComponent<LayoutElement>();
-            layout.preferredHeight = 30;
+            GameObject wordButtonInstance = Instantiate(wordButtonPrefab, ingredientTransform);
+            wordButtonInstance.GetComponentInChildren<TextMeshProUGUI>().text = ingredient.Text;
+            wordButtonInstance.GetComponent<WordButton>().SetParentPanel(this.GetComponent<Panel>());
+            wordButtonInstance.GetComponent<WordButton>().setPromt(ingredient.Text);
+            wordButtonInstance.GetComponent<WordButton>().setPromptSentence(" auf einfache, kurze aber präzise Weise, sodass jeder die grundlegende Funktion oder Bedeutung versteht.");
+            wordButtonList.Add(wordButtonInstance);
+            Debug.Log(zutatenListe.GetComponent<RectTransform>().sizeDelta);
         }
+
+        
     }
 }
+
